3b3551a3896a692ff4ba775f7f41629a36208dea
dahighsky <aakashuttkarsh29@gmail.com>
2023-05-28 17:29:40 +0530

    minor updates

--- src/home/Navbar.jsx
+++ src/home/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark">
       <nav className="navbar-brand px-5">
-        <img src="src/assets/logo.png" style={{ width: "120px" }} />
+        <img src="./src/assets/logo.png" style={{ width: "120px" }} />
       </nav>
     </nav>
   );
